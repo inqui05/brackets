843e1acb79acc3982be3609698648c922aca6fff
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-27 21:39:44 +0300

    implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets.push(bracketsConfig[i].join(''));
+  } 
+   
+  for (let i = 0; i < brackets.length;) {
+    if (str.includes(brackets[i])) {
+      str = str.replace(brackets[i], '');
+      i = 0;
+    } else {
+      i++;
+    }
+  } 
+
+  return !str;
+};
